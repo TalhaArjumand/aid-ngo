63cdfa30043e0d6cf83f29d60efd3094c5fa4f47
Inim Andrew <inimandrew@gmail.com>
2021-07-02 14:57:00 +0100

    deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,77 +1,82 @@
 const BASE_URL = "https://api.chats.cash/v1/";
 export default {
-    // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
-    ssr: false,
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: false,
 
-    env: {
-        BASE_URL
-    },
+  env: {
+    BASE_URL
+  },
 
-    head: {
-        title: "frontend-ngo-web",
-        meta: [
-            { charset: "utf-8" },
-            { name: "viewport", content: "width=device-width, initial-scale=1" },
-            { hid: "description", name: "description", content: "" }
-        ],
-        link: [
-            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-
-            {
-                rel: "stylesheet",
-                href: "https://unpkg.com/element-ui/lib/theme-chalk/index.css"
-            }
-        ],
-    },
-
-    css: ["~/assets/css/main.css", "~/assets/css/fonts.css"],
-
-    plugins: [
-        "~/plugins/global.js",
-        "~/plugins/index.js",
-        "~/plugins/axios.js",
-        "~/plugins/element-ui",
-        // "~/plugins/freshdesk.js",
-        { src: "~/plugins/vuex-persist", ssr: false }
+  head: {
+    title: "frontend-ngo-web",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    loading: {
-        color: " #17CE89",
-        name: "fading-circle",
-        continuous: true,
-        duration: 1200
-    },
+    link: [
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
 
-    components: true,
+      {
+        rel: "stylesheet",
+        href: "https://unpkg.com/element-ui/lib/theme-chalk/index.css"
+      }
+    ]
+  },
 
-    // pageTransition: {
-    //     name: 'page',
-    //     mode: 'out-in',
-    //     beforeEnter (el) {
-    //       console.log('Before enter...');
-    //     }
-    //   },
+  css: ["~/assets/css/main.css", "~/assets/css/fonts.css"],
 
-    // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-    buildModules: [],
-
-    // Modules (https://go.nuxtjs.dev/config-modules)
-    modules: ["bootstrap-vue/nuxt", "@nuxtjs/axios", "@nuxtjs/toast"],
-
-    // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-    axios: {
-        baseURL: BASE_URL,
-        https: true,
-        progress: true,
-        retry: { retries: 2 }
-    },
-  
-    toast: {
-        position: "top-right",
-        duration: 3500
-    },
-
-    // Build Configuration (https://go.nuxtjs.dev/config-build)
-    build: {
-        transpile: [/^element-ui/]
+  plugins: [
+    "~/plugins/global.js",
+    "~/plugins/index.js",
+    "~/plugins/axios.js",
+    "~/plugins/element-ui",
+    // "~/plugins/freshdesk.js",
+    { src: "~/plugins/vuex-persist", ssr: false }
+  ],
+  loading: {
+    color: " #17CE89",
+    name: "fading-circle",
+    continuous: true,
+    duration: 1200
+  },
+  build: {
+    babel: {
+      compact: true
     }
+  },
+
+  components: true,
+
+  // pageTransition: {
+  //     name: 'page',
+  //     mode: 'out-in',
+  //     beforeEnter (el) {
+  //       console.log('Before enter...');
+  //     }
+  //   },
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: ["bootstrap-vue/nuxt", "@nuxtjs/axios", "@nuxtjs/toast"],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {
+    baseURL: BASE_URL,
+    https: true,
+    progress: true,
+    retry: { retries: 2 }
+  },
+
+  toast: {
+    position: "top-right",
+    duration: 3500
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+    transpile: [/^element-ui/]
+  }
 };
