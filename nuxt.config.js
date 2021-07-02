d331191c548a7a36a5f13f796191103b1d2394fd
Inim Andrew <inimandrew@gmail.com>
2021-07-02 15:46:06 +0100

    deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,11 +40,6 @@ export default {
     continuous: true,
     duration: 1200
   },
-  build: {
-    babel: {
-      compact: true
-    }
-  },
 
   components: true,
 
@@ -77,6 +72,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    transpile: [/^element-ui/]
+    transpile: [/^element-ui/],
+    babel: { compact: true }
   }
 };
