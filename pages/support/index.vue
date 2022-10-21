36639a19cfd79ca3ad755754fb7230a3962859a9
hamicch <sanusihayatu01@gmail.com>
2022-10-21 03:10:06 +0100

    Fix: fix phone number input

--- pages/support/index.vue
+++ pages/support/index.vue
@@ -1,49 +1,95 @@
 <template>
-  <div class="main  pt-4 pb-5 transparent container">
+  <div class="main pt-4 pb-5 transparent container">
     <form class="holder" @submit.prevent="submitForm">
       <!-- First and last name -->
       <div class="row">
         <div class="form-group col">
-          <input type="text" class="form-controls" :class="{
-
-          }" name="name" id="name" placeholder="First Name" v-model="payload.contact.first_name" />
+          <input
+            type="text"
+            class="form-controls"
+            :class="{}"
+            name="name"
+            id="name"
+            placeholder="First Name"
+            v-model="payload.contact.first_name"
+          />
         </div>
         <div class="form-group col">
-          <input type="text" class="form-controls" :class="{
-
-          }" name="name" id="name" placeholder="Last Name" v-model="payload.contact.last_name" />
+          <input
+            type="text"
+            class="form-controls"
+            :class="{}"
+            name="name"
+            id="name"
+            placeholder="Last Name"
+            v-model="payload.contact.last_name"
+          />
         </div>
       </div>
 
       <!-- Email and phone number -->
       <div class="row">
         <div class="form-group col">
-          <input type="text" class="form-controls" name="email" id="email" placeholder="Email Address" :class="{
-
-          }" v-model="payload.email" @blur="$v.payload.email.$touch()" />
+          <input
+            type="text"
+            class="form-controls"
+            name="email"
+            id="email"
+            placeholder="Email Address"
+            :class="{}"
+            v-model="payload.email"
+            @blur="$v.payload.email.$touch()"
+          />
         </div>
         <div class="form-group col">
-          <input type="number" class="form-controls" :class="{
-
-          }" name="phone" id="phone" placeholder="Phone" v-model="payload.contact.phone" />
+          <vue-tel-input
+            id="phone"
+            name="phone"
+            placeholder="Phone"
+            mode="international"
+            class="form-controls phone"
+            :inputOptions="options"
+            v-model="payload.contact.phone"
+          ></vue-tel-input>
         </div>
       </div>
       <!-- Subject -->
       <div class="form-group">
-        <input type="text" class="form-controls" name="subject" id="subject" placeholder="Subject" :class="{
-          error: $v.payload.subject.$error,
-        }" v-model="payload.subject" @blur="$v.payload.subject.$touch()" />
+        <input
+          type="text"
+          class="form-controls"
+          name="subject"
+          id="subject"
+          placeholder="Subject"
+          :class="{
+            error: $v.payload.subject.$error,
+          }"
+          v-model="payload.subject"
+          @blur="$v.payload.subject.$touch()"
+        />
       </div>
       <!-- description -->
       <div class="form-group">
-        <textarea :class="{
-          error: $v.payload.description.$error,
-        }" v-model="payload.description" @blur="$v.payload.description.$touch()" rows="30"
-          placeholder="Enter something..." class="txt-area"></textarea>
+        <textarea
+          :class="{
+            error: $v.payload.description.$error,
+          }"
+          v-model="payload.description"
+          @blur="$v.payload.description.$touch()"
+          rows="30"
+          placeholder="Enter something..."
+          class="txt-area"
+        ></textarea>
       </div>
       <div class="d-flex justify-content-end w-100">
-        <Button type="submit" :has-icon="false" text="Submit" custom-styles="height:50px" :loading="loading"
-          :disabled="loading" />
+        <Button
+          type="submit"
+          :has-icon="false"
+          text="Submit"
+          custom-styles="height:50px"
+          :loading="loading"
+          :disabled="loading"
+        />
       </div>
     </form>
   </div>
@@ -53,56 +99,59 @@
 import { required } from "vuelidate/lib/validators";
 
 export default {
-  name: 'Support',
-  layout: 'dashboard',
+  name: "Support",
+  layout: "dashboard",
   data() {
     return {
       loading: false,
+      // options: { placeholder: "Phone number" },
       payload: {
-        email: '',
-        subject: '',
-        departmentId: '661286000000006907',
-        phone: '',
+        email: "",
+        subject: "",
+        departmentId: "661286000000006907",
+        phone: "",
         contact: {
-          email: '',
-          first_name: '',
-          last_name: '',
+          email: "",
+          first_name: "",
+          last_name: "",
         },
-        description: '',
+        description: "",
       },
-
-    }
+    };
   },
 
   validations: {
     payload: {
       email: {
-        required
+        required,
       },
       subject: {
-        required
+        required,
       },
       description: {
-        required
-      }
-    }
+        required,
+      },
+    },
   },
 
   methods: {
     async submitForm() {
       try {
-
-        this.loading = true
-        this.payload.contact.email = this.payload.email
+        this.loading = true;
+        this.payload.contact.email = this.payload.email;
 
         this.$v.payload.$touch();
 
-        if (this.$v.payload.email.$error === true || this.$v.payload.subject.$error === true || this.$v.payload.description.$error === true) {
+        if (
+          this.$v.payload.email.$error === true ||
+          this.$v.payload.subject.$error === true ||
+          this.$v.payload.description.$error === true
+        ) {
           return (this.loading = false);
         }
 
         const response = await this.$axios.post(
-          'organisation/zoho-cretate-ticket',
+          "organisation/zoho-cretate-ticket",
           this.payload
         );
 
@@ -110,15 +159,14 @@ export default {
           this.loading = false;
           this.$toast.success(response.message);
         }
-
       } catch (error) {
         console.log(error);
         this.$toast.error(error?.response?.data?.message);
         this.loading = false;
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -161,5 +209,14 @@ export default {
 
 .txt-area:focus {
   border: 1px solid var(--primary-color) !important;
+}
+</style>
+
+<style>
+.phone {
+  border: none;
+  background-color: none;
+  border-radius: 5px;
+  padding: 0rem 0.7rem;
 }
 </style>
