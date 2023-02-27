1ccdee0e204a8dab1ea9068c560a79fedad3f475
Dasaolu Daniel <yungdansa@gmail.com>
2023-02-27 07:33:51 +0000

    Feature/item campaign

--- pages/campaigns/index.vue
+++ pages/campaigns/index.vue
@@ -36,7 +36,7 @@
 
       <div class="ml-auto mx-3">
         <Button
-          v-if="section === 'campaigns'"
+          v-if="section === 'campaigns' || section === 'items'"
           text="Create campaign"
           custom-styles="height:50px"
           @click="$bvModal.show('new-campaign')"
@@ -56,12 +56,26 @@
       <b-tabs content-class="mt-1" id="profile-tab" nav-class nav-wrapper-class>
         <!-- Campaigns tab here -->
         <b-tab
-          title="Campaigns"
+          title="Cash Campaigns"
           :active="section === 'campaigns'"
           class="nav-links"
           @click="handleTabClick('campaigns')"
         >
           <CampaignTable
+            :resultQuery="resultQuery"
+            :loading="$fetchState.pending"
+            :id="id"
+            @reload="$fetch"
+          />
+        </b-tab>
+
+        <b-tab
+          title="Item Campaign"
+          :active="section === 'items'"
+          class="nav-links"
+          @click="handleTabClick('items')"
+        >
+          <ItemCampaigns
             :resultQuery="resultQuery"
             :loading="$fetchState.pending"
             :id="id"
@@ -89,17 +103,19 @@
 <script>
 import CampaignTable from "~/components/tables/campaigns/all-campaigns.vue";
 import CampaignForms from "~/components/tables/campaigns/CampaignForms.vue";
+import ItemCampaigns from "~/components/tables/campaigns/ItemCampaigns.vue";
 import { mapGetters } from "vuex";
 
 export default {
   layout: "dashboard",
-  components: { CampaignTable, CampaignForms },
+  components: { CampaignTable, CampaignForms, ItemCampaigns },
 
   data: () => ({
     id: "",
     searchQuery: "",
     campaigns: [],
     forms: [],
+    items: [],
   }),
 
   computed: {
@@ -110,7 +126,7 @@ export default {
     },
 
     resultQuery() {
-      const data = this.section === "campaigns" ? this.campaigns : this.forms;
+      const data = this[this.section];
       if (this.searchQuery) {
         return data.filter((item) =>
           item?.title.toLowerCase().includes(this.searchQuery.toLowerCase())
@@ -137,6 +153,15 @@ export default {
 
     if (responseTwo.status === "success") {
       this.campaigns = responseTwo.data;
+    }
+
+    // fetch item campaign's response
+    const responseThree = await this.$axios.get(
+      `/organisations/${this.id}/campaigns/all?type=item`
+    );
+
+    if (responseThree.status === "success") {
+      this.items = responseThree.data;
     }
   },
 
