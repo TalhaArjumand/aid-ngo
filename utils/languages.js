2711bc3671176614de82c085addc0bf05355ec1c
alphajeez <chimeremezechukwudire@gmail.com>
2023-06-08 00:04:42 +0100

    added language switcher

---
+++ utils/languages.js
@@ -0,0 +1,75 @@
+// Refrence Here: https://github.com/i7eo/google-translate-select/tree/backup/master#props
+const languages = [
+  {
+    code: "en",
+    name: "English",
+    cname: "英语",
+    ename: "English",
+  },
+  {
+    code: "af",
+    name: "Afrikaans",
+    cname: "南非语",
+    ename: "Afrikaans",
+  },
+
+  {
+    code: "ar",
+    name: "العربية",
+    cname: "阿拉伯语",
+    ename: "Arabic",
+  },
+  {
+    code: "zh-CN",
+    name: "Chinese",
+    cname: "中文 (简体)",
+    ename: "Chinese",
+  },
+  {
+    code: "nl",
+    name: "Nederlands",
+    cname: "荷兰语",
+    ename: "Dutch",
+  },
+  {
+    code: "fr",
+    name: "Français",
+    cname: "法语",
+    ename: "French",
+  },
+
+  {
+    code: "de",
+    name: "Deutsch",
+    cname: "德语",
+    ename: "German",
+  },
+  //   {
+  //     code: "el",
+  //     name: "Ελληνικά",
+  //     cname: "希腊语",
+  //     ename: "Greek",
+  //   },
+
+  {
+    code: "it",
+    name: "Italiano",
+    cname: "意大利语",
+    ename: "Italian",
+  },
+  {
+    code: "pt",
+    name: "Português",
+    cname: "葡萄牙语",
+    ename: "Portuguese",
+  },
+
+  {
+    code: "es",
+    name: "Español",
+    cname: "西班牙语",
+    ename: "Spanish",
+  },
+];
+
+export default languages;
